1b36305c892c1003e80e84c87ece49df3547eb2f
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-08-09 20:46:58 +0900

    feat: add payment

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'users.apps.UsersConfig',
     'lms.apps.LmsConfig',
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
-from .models import User
+from users.models import User, Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'email', 'phone', 'city', 'avatar')
+        fields = ['id', 'email', 'phone', 'city', 'avatar', 'payments']
         read_only_fields = ('email',)
