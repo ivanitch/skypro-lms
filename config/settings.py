9f66c04c6340a7e91b31010a8f3993509a4826d7
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-08-30 13:57:19 +0900

    feat: add celery

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
+    'django_celery_beat',
     'users.apps.UsersConfig',
     'lms.apps.LmsConfig',
 ]
@@ -165,3 +166,29 @@ MEDIA_ROOT = BASE_DIR / 'media'
 PAGE_SIZE = int(os.getenv('PER_PAGE', 15))
 
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_FROM', EMAIL_HOST_USER)
+
+# Celery
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_ENABLE_UTC = False
+
+# Celery Beat Schedule
+CELERY_BEAT_SCHEDULE = {
+    'check-inactive-users-daily': {
+        'task': 'users.tasks.check_inactive_users',
+        'schedule': timedelta(days=1),  # Запуск каждый день
+    },
+}
--- lms/admin.py
+++ lms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from lms.models import Course, Lesson
+from users.models import Payment
 
 
 @admin.register(Course)
@@ -13,3 +14,9 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     ordering = ('title',)
     list_display = ('title', 'preview', 'description', 'video_link', 'course')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    ordering = ('amount',)
+    list_display = ('id', 'amount', 'user', 'paid_course', 'paid_lesson')
